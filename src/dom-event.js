769c22a048c5622fcbfa4f1ce0ef5add099f7fe2
IntPtr <lxy.lixiaoyan@gmail.com>
2015-06-28 21:44:53 +0800

    split event listener handler to another file

---
+++ src/dom-event.js
@@ -0,0 +1,17 @@
+var dispatcher = require("./dispatcher");
+var eventStore = require("./event-store");
+
+function eventHandler(e) {
+  var handler = eventStore.get(this)[e.type];
+  if(handler) {
+    handler.call(this, e);
+  }
+}
+
+event.on(dispatcher, "dom-event:add", function(dom, name) {
+  dom.addEventListener(name, eventHandler, false);
+});
+
+event.on(dispatcher, "dom-event:remove", function(dom, name) {
+  dom.removeEventListener(name, eventHandler, false);
+});
