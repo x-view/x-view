0653de92c195a5006a2ae644c3553a26e17f66d0
IntPtr <lxy.lixiaoyan@gmail.com>
2015-06-30 19:59:37 +0800

    update build flow for supporting builtin tags

---
+++ tags.js
@@ -0,0 +1,1 @@
+require("./tags/raw");
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var webpack = require("webpack");
 
 var config = {
   context: __dirname,
-  entry: "./index.js",
+  entry: ["./index.js", "./tags.js"],
   output: {
     path: __dirname + "/dist",
     filename: "x-view.js",
