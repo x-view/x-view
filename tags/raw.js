5779d524b444156ba7427cb86b78080ddf8cac27
IntPtr <lxy.lixiaoyan@gmail.com>
2015-06-30 19:55:52 +0800

    add tag <x-raw>

---
+++ tags/raw.js
@@ -0,0 +1,15 @@
+var x = require("../index");
+
+var Raw = x.createClass({
+  propTypes: {
+    html: x.type.string
+  },
+  init: function() {
+    x.event.on(this, "update", this.updateHTML);
+  },
+  updateHTML: function() {
+    this.root.innerHTML = this.props.html;
+  }
+});
+
+x.register("x-raw", Raw);
